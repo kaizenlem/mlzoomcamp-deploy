228fe81ff0ba5f3ac49254d8af6a22e243a5a446
kaizenlem <49950579+kaizenlem@users.noreply.github.com>
2025-10-28 16:25:24 +0000

    serve and main py

---
+++ serve.py
@@ -0,0 +1,20 @@
+import pickle
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Client(BaseModel):
+    lead_source: str
+    number_of_courses_viewed: int
+    annual_income: float
+
+# Load the pipeline
+with open("pipeline_v1.bin", "rb") as f_in:
+    pipeline = pickle.load(f_in)
+
+app = FastAPI()
+
+@app.post("/predict")
+def predict(client: Client):
+    X = client.dict()
+    prob = pipeline.predict_proba([X])[0, 1]
+    return {"probability": prob}
